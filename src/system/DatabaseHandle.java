c53c8710fd582f44bde041921b8162100f011061
Scott Simmons <scottsimmons345@gmail.com>
2018-12-11 15:17:37 +0000

    Tested Database functions in Login

--- src/system/DatabaseHandle.java
+++ src/system/DatabaseHandle.java
@@ -7,12 +7,15 @@ import java.sql.*;
  */
 public class DatabaseHandle {
 
+        // URL specifying driver to load the database
+        private final static String URL = "jdbc:sqlite:database/LibDB.db";
+
         /**
          * Executes a provided query on the database
          * @param query The SQL query to run on the database
          * @return ResultSet of data from the database
          */
-        public ResultSet dbQuery(String query) {
+        public static ResultSet dbQuery(String query) {
 
                 try {
                         // Get connection
@@ -34,11 +37,8 @@ public class DatabaseHandle {
                 Connection con = null;
 
                 try {
-                        // DB Parameters
-                        String url = "jdbc:sqlite:database/LibDB.db";
-
                         // Create a connection
-                        con = DriverManager.getConnection(url);
+                        con = DriverManager.getConnection(URL);
 
                         System.out.println("Connection Established!");
                 } catch (SQLException e) {
@@ -49,8 +49,6 @@ public class DatabaseHandle {
         }
 
         public static void main(String[] args) {
-                connect();
+
         }
-
-
 }
