6fe1b1a3ddef1da5d4400c7cfe4c5fa7adb72cd1
Scott Simmons <scottsimmons345@gmail.com>
2018-12-11 14:49:36 +0000

    Added dbQuery function

--- src/system/DatabaseHandle.java
+++ src/system/DatabaseHandle.java
@@ -8,12 +8,33 @@ import java.sql.*;
 public class DatabaseHandle {
 
         /**
+         * Executes a provided query on the database
+         * @param query The SQL query to run on the database
+         * @return ResultSet of data from the database
+         */
+        public ResultSet dbQuery(String query) {
+
+                try {
+                        // Get connection
+                        Connection con = connect();
+                        PreparedStatement sql = con.prepareStatement(query);
+
+                        // Return results
+                        return sql.executeQuery();
+                } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                        return null;
+                }
+        }
+
+        /**
          * Connect to the database
          */
-        private static void connect() {
+        private static Connection connect() {
                 Connection con = null;
 
                 try {
+                        // DB Parameters
                         String url = "jdbc:sqlite:database/LibDB.db";
 
                         // Create a connection
@@ -22,15 +43,9 @@ public class DatabaseHandle {
                         System.out.println("Connection Established!");
                 } catch (SQLException e) {
                         System.out.println(e.getMessage());
-                } finally {
-                        try {
-                                if (con == null) {
-                                        con.close();
-                                }
-                        } catch (SQLException ex) {
-                                System.out.println(ex.getMessage());
-                        }
                 }
+
+                return con;
         }
 
         public static void main(String[] args) {
