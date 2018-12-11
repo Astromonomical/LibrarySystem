ab93c4fc46f39fe7f22b311b3d2bb36651cc98e7
Scott Simmons <scottsimmons345@gmail.com>
2018-12-11 14:10:11 +0000

    Working on verify code

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,6 +1,11 @@
 package application;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -13,6 +18,22 @@ import java.io.IOException;
  */
 public class LoginController extends Application {
 
+        // Login button
+        @FXML
+        private JFXButton loginBtn;
+
+        // Username text field
+        @FXML
+        private JFXTextField usernameTxt;
+
+        // Password text field
+        @FXML
+        private JFXPasswordField passwordTxt;
+
+        /**
+         * Launches the login form
+         * @param args
+         */
         public static void main(String[] args) {
                 launch(args);
         }
@@ -25,14 +46,25 @@ public class LoginController extends Application {
         public void start(Stage primaryStage) {
                 try {
                         BorderPane root =
-                                FXMLLoader.load(getClass().getClassLoader().getResource("application/LoginForm.fmxl"));
+                                FXMLLoader.load(getClass().getClassLoader().getResource("application/LoginForm.fxml"));
 
                         // Load root into the stage
                         Scene scene = new Scene(root);
                         Stage stage = new Stage();
                         stage.setScene(scene);
+                        stage.show();
                 } catch (IOException e) {
                         e.printStackTrace();
                 }
+        }
+
+        /**
+         * Handles logging in
+         * @param event
+         */
+        @FXML
+        private void handleLoginBtn(ActionEvent event) {
+                LoginFunctions.verify(usernameTxt.getText(),
+                        passwordTxt.getText());
         }
 }
--- src/application/LoginFunctions.java
+++ src/application/LoginFunctions.java
@@ -1,7 +1,12 @@
 package application;
 
 /**
- * 
+ * Handles the functions and validation of the login form
  */
 public class LoginFunctions {
+
+        public static boolean verify(String username, String password) {
+                
+                return false;
+        }
 }
